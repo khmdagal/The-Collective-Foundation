1a86f418d8df819998955189423b4f849f62d13c
Doris-Siu <doris.siu.my@gmail.com>
2023-03-09 22:42:53 +0000

    Update the Header component and Homepage

--- client/src/shared-modules/Header.js
+++ client/src/shared-modules/Header.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return (
-    <div>
-      <h1>Header place holder</h1>
-    </div>
-  );
+	return (
+		<div >
+			<header>
+				<a href="#">Collective Foundation</a>
+
+				<nav>
+					<ul>
+						<li>
+							<Link to="/page1">Page 1</Link>
+						</li>
+						<li>
+							<Link to="/page2">Page 2</Link>
+						</li>
+						<li>
+							<Link to="/admin">Admin</Link>
+						</li>
+						<li>
+							<a href="https://twitter.com/">Twitter</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/">Linkedin</a>
+						</li>
+						<li>
+							<a href="https://www.facebook.com/">FB</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	);
 }
 
 export default Header;
