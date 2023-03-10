e822003f3ae3e44fdf2c7561d4e06d8644521033
khmdagal <khmdagal@gmail.com>
2023-03-10 12:25:45 +0000

    Update the footer Component (ticket#13)

--- client/src/shared-modules/Footer.js
+++ client/src/shared-modules/Footer.js
@@ -1,11 +1,57 @@
+
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer place holder</h1>
-    </div>
-  );
+		<Card className="card-footer">
+			<section className="footer-address-section">
+				<img src="#" alt="The Collective Foundation logo" />
+				<h3>The Collective Foundation</h3>
+				<ul>
+					<li>Address</li>
+					<li>City & Postcod</li>
+					<li>Contact number</li>
+				</ul>
+			</section>
+			<section className="footer-pageAndSocialmedia-section">
+				<div className="footer-pages">
+					<h2>Our Work</h2>
+					<ul>
+						<li>
+							<a href="#">About</a>
+						</li>
+
+						<li>
+							<a href="#">Initiatives</a>
+						</li>
+
+						<li>
+							<a href="#">Take Action</a>
+						</li>
+					</ul>
+				</div>
+
+				<div className="footer-socialmedia">
+					<h2>Follow Us</h2>
+
+					<ul>
+						<li>
+							<a href="#">Twitter</a>
+						</li>
+
+						<li>
+							<a href="#">LinkedIn</a>
+						</li>
+
+						<li>
+							<a href="#">Facebook</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</Card>
+	);
 }
 
 export default Footer;
