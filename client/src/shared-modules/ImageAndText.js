3cfbcd9f4acc97df8a1408e0c85543e34d08336b
Jihad Omer <jhjahony@gmail.com>
2023-03-11 08:20:47 +0000

    add more styling

--- client/src/shared-modules/ImageAndText.js
+++ client/src/shared-modules/ImageAndText.js
@@ -1,18 +1,29 @@
 import React from "react";
 import homePageImage1 from "../Images/home-page-image1.jpg";
+import   "../shared-modules/ImageAndText.css";
 
 
 
 function ImageAndText() {
   return (
-    <div>
-      <h1>ImageAndText</h1>
-      <p>lorem ipsum dan wpwejh refoij dh o</p> 
-      <img src={homePageImage1}></img>
-      <button>LEARN MORE</button>
-
-    </div>
-  );
+	
+		<div className="container">
+			<div className="image-container">
+				<img src={homePageImage1} alt='img'></img>
+			</div>
+			<div className="content-container">
+				<div className="content">
+					<h1>Changing the word is possible. we've done it before </h1>
+					<p >
+						Our Leadership team bring years of experince to bear on the greatest
+						challenge of our time. we're results driven with a proven record of
+						previous successes
+					</p>
+					<button className="button">LEARN MORE</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ImageAndText;
