055ffccfbd75fa6e17c24eac2459e08e32573f5c
Jihad Omer <jhjahony@gmail.com>
2023-03-15 19:11:19 +0000

    Add some Format

--- client/src/shared-modules/HeroBanner.js
+++ client/src/shared-modules/HeroBanner.js
@@ -2,12 +2,14 @@ import heroimg from "../Images/Hero-banner-image1.jpg";
 import "../shared-modules/HeroBanner.css";
 
 const HeroBanner = () => {
-  return (
+	return (
 		<>
 			<div className="image-container">
 				<img src={heroimg} alt="img"></img>
-				<p>Protecting natural habitats from extinction.</p>
-				<button>Learn More</button>
+				<div className="hero-banner-content-overlay">
+					<p>Protecting natural habitats from extinction.</p>
+					<button>Learn More</button>
+				</div>
 			</div>
 		</>
 	);
