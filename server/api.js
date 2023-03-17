95d4938e152f8e62998c80fd32ba2a9ada026e3b
khmdagal <khmdagal@gmail.com>
2023-03-17 20:18:16 +0000

    Create Table queries and endpoints

--- server/api.js
+++ server/api.js
@@ -1,3 +1,9 @@
+/* eslint-disable semi */
+/* eslint-disable arrow-parens */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable quotes */
 import { Router } from "express";
 import db from "./db";
 import logger from "./utils/logger";
@@ -9,14 +15,63 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 router.get("/pages", (req, res) => {
-	
-	db.query('select * from pages')
-	.then(pages => res.status(200).json(pages.rows))
-	.catch(err=>
-		{console.error(err)
-		res.status(500).send(err)
-		})
+	db.query("select * from pages")
+		.then((pages) => res.status(200).json(pages.rows))
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send(err);
+		});
+});
 
+
+// this is Home endpoint
+router.get("/home", async (req, res) => {
+	try {
+		const response =
+			await db.query(`SELECT p.page_title, i.text, i.image, i.button, i.hasButton 
+							FROM pages AS p
+							INNER JOIN modules AS m ON p.page_id = m.page_id
+							INNER JOIN imageAndTexts AS i ON m.record_id = i.imageText_id
+							WHERE p.page_title = 'Home' `);
+		res.json(response.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+});
+
+
+// this is About page endpoint
+router.get("/about", async (req, res) => {
+	try {
+		const response =
+			await db.query(`SELECT p.page_title, i.text, i.image, i.button, i.hasButton 
+							FROM pages AS p
+							INNER JOIN modules AS m ON p.page_id = m.page_id
+							INNER JOIN imageAndTexts AS i ON m.record_id = i.imageText_id
+							WHERE p.page_title = 'About' `);
+		res.json(response.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+});
+
+
+// this is Contact page endpoint
+router.get("/contact", async (req, res) => {
+	try {
+		const response =
+			await db.query(`SELECT p.page_title, i.text, i.image, i.button, i.hasButton 
+							FROM pages AS p
+							INNER JOIN modules AS m ON p.page_id = m.page_id
+							INNER JOIN imageAndTexts AS i ON m.record_id = i.imageText_id
+							WHERE p.page_title = 'Contact' `);
+		res.json(response.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
 });
 
 
