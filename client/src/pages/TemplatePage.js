f496b7de3a4ff890343faffd11e054ec30cd504d
Shima <shimaadnanbasmenj@gmail.com>
2023-03-21 14:49:56 +0000

    module work in each page

--- client/src/pages/TemplatePage.js
+++ client/src/pages/TemplatePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useState,useEffect } from "react";
 import ImageAndText from '../shared-modules/ImageAndText';
+import Header from '../shared-modules/Header'
+import Footer from '../shared-modules/Footer'
 
 export default function TemplatePage({pagetitle}) {
     const [content, setContent] = useState([]);
-console.log(pagetitle)
     async function fetchPagesAPI() {
       const page = await fetch(`/api/pages/${pagetitle}`);
       const response = await page.json();
@@ -14,22 +15,19 @@ console.log(pagetitle)
     useEffect(() => {
        fetchPagesAPI();
     }, []);
-
+console.log(content);
     console.log(content);
     if (!content.title) {
       return <div>No content available for {pagetitle}.</div>;
     }
-// {content.modules && content.modules.map((item)=>{if(item.type==='imageAndTexts')return <div><ImageAndText/></div> })}
 
-
-
-     if(content.modules[0].type==='imageAndTexts'){
-      return <div><ImageAndText/></div>
-    }
   return (
 
     <div>
-       <h1>Hello{content.title}</h1>
+      <Header/>
+       
+       {content.modules.map(module=>{return (<div> {module.type==='imageAndTexts' && <ImageAndText/>}</div>)})}
+       <Footer/>
     </div>
   )
 }
