3b9033df8042de861fdaac598613f1ab72c52c2b
Jihad Omer <jhjahony@gmail.com>
2023-03-21 14:06:36 +0000

    delete exta modules

--- client/src/pages/Page1.js
+++ client/src/pages/Page1.js
@@ -2,8 +2,6 @@
 import React from "react";
 import Header from "../shared-modules/Header";
 import Footer from "../shared-modules/Footer";
-import Module1 from "../shared-modules/Module1";
-import Module2 from "../shared-modules/Module2";
 import ImageAndText from "../shared-modules/ImageAndText";
 import HeroBanner from "../shared-modules/HeroBanner";
 
@@ -14,8 +12,6 @@ export default function Page1() {
 			<Header />
 			<HeroBanner />
 			<ImageAndText />
-			<Module1 />
-			<Module2 />
 
 			<Footer />
 		</div>
