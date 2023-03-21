910c31f8e574e0b4e2b5389b7a7345bb31ff5925
Shima <shimaadnanbasmenj@gmail.com>
2023-03-21 13:58:34 +0000

    textandimage  module on page home

--- client/src/pages/TemplatePage.js
+++ client/src/pages/TemplatePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState,useEffect } from "react";
+import ImageAndText from '../shared-modules/ImageAndText';
 
 export default function TemplatePage({pagetitle}) {
     const [content, setContent] = useState([]);
@@ -17,6 +18,13 @@ console.log(pagetitle)
     console.log(content);
     if (!content.title) {
       return <div>No content available for {pagetitle}.</div>;
+    }
+// {content.modules && content.modules.map((item)=>{if(item.type==='imageAndTexts')return <div><ImageAndText/></div> })}
+
+
+
+     if(content.modules[0].type==='imageAndTexts'){
+      return <div><ImageAndText/></div>
     }
   return (
 
