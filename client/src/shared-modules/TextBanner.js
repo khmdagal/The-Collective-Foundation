389c6b7a907028141bf86da329a74820f3e99c1d
Jihad Omer <jihadomer940@gmail.com>
2023-03-23 23:56:24 +0000

    textBanner fixed

--- client/src/shared-modules/TextBanner.js
+++ client/src/shared-modules/TextBanner.js
@@ -1,22 +1,20 @@
 import "../shared-modules/TextBanner.css";
 
+const leftText = "Climate change threatens every part of the planet. It’s a global problem that requires global cooperation.";
+const rightText =
+	"Our mission is to create international consensus around the climate emergency, as well a shared plan for saving the planet’s most exceptional wild places";
+
  const TextBanner = () => {
-	return (
-		<>
-			<div className="text-banner-container">
-				<p className="left-text">
-					Climate change threatens every part of the planet. It’s a global
-					problem that requires global cooperation.lkhl
-				</p>
-				<p className="right-text">
-					Our mission is to create international consensus around the climate
-					emergency, as well a shared plan for saving the planet’s most
-					exceptional wild places.
-				</p>
-			</div>
-		</>
-	);
-};
+		return (
+			<>
+				<div className="text-banner-container">
+					<p className="left-text">{leftText}</p>
+					<p className="right-text">{rightText}</p>
+
+				</div>
+			</>
+		);
+ };
 
 
 export default TextBanner;
