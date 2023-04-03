34ca020fcf82b965c3c7303872d195a99b04ce88
khmdagal <khmdagal@gmail.com>
2023-04-03 23:25:41 +0100

    Edit Image names

--- server/utils/latest.sql
+++ server/utils/latest.sql
@@ -57,10 +57,10 @@ VALUES
 (3,'textBanner',3);
 INSERT INTO imageAndTexts (text_header,text_body,image,button,hasButton,imagetext_direction)
 VALUES
-('Changing the world is possible. We’ve done it before.','Our leadership team bring years of experience to bear on the greatest challenge of our time. We’re results driven, with a proven record of previous successes.','image1.jpg','Learn More',true,true),
-('Ready to take the next step? ','This is a movement of billions. Whether you’re most comfortable contributing time to help achieve our advocacy goals, money to help us grow, or energy to put political pressure on our governments to change, we need you on our team.','image2.jpg','Take Action',true,false),
-('Invest in your relationship with yourself','We all have a tendency to put others first, but in doing so, we can sometimes neglect our relationship with ourselves. Let me be your advocate and show you a kinder, gentler way to treat the most important person in your life.','image3.jpg','Learn More',true,true),
-('Ready to take the next step? ','This is a movement of billions. Whether you’re most comfortable contributing time to help achieve our advocacy goals, money to help us grow, or energy to put political pressure on our governments to change, we need you on our team.','image4.jpg','Take Action',true,false),
+('Changing the world is possible. We’ve done it before.','Our leadership team bring years of experience to bear on the greatest challenge of our time. We’re results driven, with a proven record of previous successes.','textAndImage-image1.jpg','Learn More',true,true),
+('Ready to take the next step? ','This is a movement of billions. Whether you’re most comfortable contributing time to help achieve our advocacy goals, money to help us grow, or energy to put political pressure on our governments to change, we need you on our team.','textAndImage-image2.jpg','Take Action',true,false),
+('Invest in your relationship with yourself','We all have a tendency to put others first, but in doing so, we can sometimes neglect our relationship with ourselves. Let me be your advocate and show you a kinder, gentler way to treat the most important person in your life.','textAndImage-image3.jpg','Learn More',true,true),
+('Ready to take the next step? ','This is a movement of billions. Whether you’re most comfortable contributing time to help achieve our advocacy goals, money to help us grow, or energy to put political pressure on our governments to change, we need you on our team.','textAndImage-image4.jpg','Take Action',true,false),
 ('Take Action','Ready to take the next step? You can become a contributor to our cause, or participate yourself.','image5.jpg','Find Out How',true,false);
 
 INSERT INTO textBanner(textBold,textNormal, background)
@@ -71,6 +71,6 @@ VALUES
 
 INSERT INTO heroBanner(hero_image,hero_text)
 VALUES 
-('image1.jpg',' They inspire everyone to care for the planet.'),
-('image2.jpg','Starts With You'),
-('image3.jpg','Protecting natural habitats from extinction.');
+('herobanner-image1.jpg',' They inspire everyone to care for the planet.'),
+('herobanner-image2.jpg','Starts With You'),
+('herobanner-image3.jpg','Protecting natural habitats from extinction.');
